530f8d7fc89888fb3a424ad0bf281f61a7ddeeca
spdy999 <1a1a1a.262626.0010@gmail.com>
2019-05-22 20:47:54 +0700

    remove no-inferrable-types of tslint, test constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'Pun';
+  name: string = 'Pun';
+  constructor() {
+    this.changeName('John');
+  }
+
+  changeName(name: string): void {
+    this.name = name;
+  }
 }
